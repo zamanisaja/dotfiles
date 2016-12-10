4c059b67dfcae7e156a6a39d0ca521ba64e912bd
zamanisaja <zamani.saja@gmail.com>
2016-12-10 14:44:13 +0330

    auto 2016-12-10-14-44

--- .config/mpv/scripts/delete-file.lua
+++ .config/mpv/scripts/delete-file.lua
@@ -4,10 +4,8 @@ filename = nil
 function del(  )
     t = {}
     if filename == mp.get_property("path") then
-        -- dir , name = mputils.split_path(filename)
         name = filename:gsub ('%..*$' , '')
         command = "deleting file " .. name
-        -- print (command)
         t.args = {"rm" , filename , name .. ".en." .. "srt", name .. ".es." .. "srt", name .. ".fa." .. "srt" }
         res = mputils.subprocess(t) 
         mp.osd_message(command, 5)
@@ -19,4 +17,13 @@ function help(  )
     mp.add_timeout(3,del)
 end
 
-mp.add_forced_key_binding("D", "delete-file", help)
+function shift_del()
+    filename = mp.get_property("path")
+    t = {}
+    name = filename:gsub ('%..*$' , '')
+    t.args = {"rm" , filename , name .. ".en." .. "srt", name .. ".es." .. "srt", name .. ".fa." .. "srt" }
+    res = mputils.subprocess(t)
+end
+
+mp.add_forced_key_binding("del", "delete-file", help)
+mp.add_forced_key_binding("Shift+del", "shift-delete-file", shift_del)
